5625a904088efcf147d8204bb201ffd996b23703
kjacoby3 <kjacoby3@ycp.edu>
2023-10-23 22:21:14 -0400

    initialize hardware route

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+
+const router = require('./routes/index.js');
 
 const app = express();
 
-const port = process.env.PORT;
+app.use(router);
+
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
   res.status(200).send('OK');
