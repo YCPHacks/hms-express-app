222d4064582f56f7935361082e9a769cd779f5ea
kjacoby3 <kjacoby3@ycp.edu>
2023-10-30 21:07:59 -0400

    view hardware list for attendees

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const getOrganizerInventoryData = require('../utils/organizer-inventory-data.js');
+// const getOrganizerInventoryData = require('../utils/organizer-inventory-data.js');
 const getAttendeeInventoryData = require('../utils/attendee-inventory-data.js');
 const express = require('express');
 const { fetch } = require('undici');
@@ -16,19 +16,35 @@ router.get('/hardware', async (req, res) => {
     res.status(200).send(result);
 });
 
-router.get('/organizer-inventory', async (req, res) => {
-    const { page, limit, search, filter } = req.query;
-    res.locals = { ...await getOrganizerInventoryData({ page, limit, search, filter}) };
-    res.locals.title = "Hardware Inventory"
-    res.status(200).render('organizer-inventory');
-});
-
 router.get('/attendee-inventory', async (req, res) => {
-    const { page, limit, search, filter } = req.query;
+    const { page, limit, search } = req.query;
+    const categories = [req.query.categories];
+    const statuses = [req.query.statuses];
+
     console.log(req.query);
-    res.locals = { ...await getAttendeeInventoryData({ page, limit, search, filter}) };
+    res.locals = { ...await getAttendeeInventoryData({ page, limit, search, categories, statuses}) };
     res.locals.title = "Hardware Inventory"
     res.status(200).render('attendee-inventory');
 });
+
+// router.get('/organizer-inventory', async (req, res) => {
+//     const { page, limit, search } = req.query;
+//     const categories = [req.query.categories];
+//     const statuses = [req.query.statuses];
+    
+//     res.locals = { ...await getOrganizerInventoryData({ page, limit, search, categories, statuses}) };
+//     res.locals.title = "Hardware Inventory"
+//     res.status(200).render('organizer-inventory');
+// });
+
+// router.get('/inventory-management', async (req, res) => {
+//     const { page, limit, search } = req.query;
+//     const categories = [req.query.categories];
+//     const statuses = [req.query.statuses];
+    
+//     res.locals = { ...await getOrganizerInventoryData({ page, limit, search, categories, statuses}) };
+//     res.locals.title = "Inventory Management"
+//     res.status(200).render('inventory-management');
+// });
 
 module.exports = router;
--- utils/attendee-inventory-data.js
+++ utils/attendee-inventory-data.js
@@ -1,14 +1,13 @@
 const { fetch } = require('undici');
 // These are the column headers for the table
-const columns = ["Name", "Tag", "Category", "Status"];
+const columns = ["Name", "Tag", "Category"/* , "Status" */];
 
-const categories = ["PC", "VR Headset"];
-const statuses = ["Checked-In", "Checked-Out", "Unavailable"];
+const categoryOptions = ["PC", "VR Headset"];
+const statusOptions = ["Checked-In", "Checked-Out", "Unavailable"];
 
 // This is the data for the table
 async function getData({ page, limit, search, filter}) {
     const queryString = `?page=${page}&limit=${limit}&search=${search}&filter=${filter}`;
-    // const equipment = await database.find('equipment');
     const hardware = await fetch(process.env.HARDWARE_MANAGEMENT_URL + 'hardware' + queryString, {
         method: 'GET',
         headers: {
@@ -23,16 +22,19 @@ async function getData({ page, limit, search, filter}) {
 };
 
 // This is compiling the data into a single object
-async function getAttendeeInventoryData({ page = 1, limit = 20, search = null, filter = null}) {
+async function getAttendeeInventoryData({ page = 1, limit = 20, search = '', categories = [], statuses = []}) {
+    console.log(search);
     return {
         columns,
-        data: await getData({ page, limit, search, filter}),
+        data: await getData({ page, limit, search, categories, statuses}),
         currentPage: page,
         limit,
         search,
-        filter,
         categories,
         statuses,
+        categoryOptions,
+        statusOptions,
+        baseUrl: '/attendee-inventory'
     };
 }
 
