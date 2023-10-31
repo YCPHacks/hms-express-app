e4cd6993d89f27a4126b6c07cc189222a6a2696c
kjacoby3 <kjacoby3@ycp.edu>
2023-10-31 13:41:01 -0400

    list status and mock checkout functionality

---
+++ utils/hardware-data.js
@@ -0,0 +1,23 @@
+async function getData(hardwareId) {
+    const hardware = await fetch(process.env.HARDWARE_MANAGEMENT_URL + 'hardware/' + hardwareId, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(res => res.json());
+    return hardware;
+}
+
+// This is compiling the data into a single object
+async function getHardwareData(id) {
+    const data = await getData(id);
+    return {
+        id: data.id,
+        name: data.name,
+        tag: data.tag,
+        category: data.category,
+    };
+}
+
+// This is exporting the object as a module to be imported elsewhere
+module.exports = getHardwareData;
--- utils/organizer-inventory-data.js
+++ utils/organizer-inventory-data.js
@@ -1,15 +1,14 @@
 const { fetch } = require('undici');
 // These are the column headers for the table
-const columns = [" ","Name", "Tag", "Category", "Status", "User", "Date"];
+const columns = [" ","Name", "Tag", "Category", "Status", "User Name", "Time"];
 
-const categories = ["PC", "VR Headset"];
-const statuses = ["Checked-In", "Checked-Out", "Unavailable"];
+const categoryOptions = ["PC", "VR Headset"];
+const statusOptions = ["Checked-In", "Checked-Out", "Unavailable"];
 
 // This is the data for the table
 async function getData({ page, limit, search, filter}) {
     const queryString = `?page=${page}&limit=${limit}&search=${search}&filter=${filter}`;
-    // const equipment = await database.find('equipment');
-    const hardware = await fetch(process.env.HARDWARE_MANAGEMENT_URL + 'hardware' + queryString, {
+    const hardware = await fetch(process.env.HARDWARE_MANAGEMENT_URL + 'hardware_rentals_current' + queryString, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json'
@@ -22,25 +21,19 @@ async function getData({ page, limit, search, filter}) {
     return hardware;
 };
 
-// These are the options for the table
-const sortable = true;
-const searchable = true;
-const filterable = true;
-
 // This is compiling the data into a single object
-async function getOrganizerInventoryData({ page = 1, limit = 20, search = null, filter = null}) {
+async function getOrganizerInventoryData({ page = 1, limit = 20, search = '', categories = [], statuses = []}) {
     return {
         columns,
-        data: await getData({ page, limit, search, filter}),
+        data: await getData({ page, limit, search, categories, statuses}),
         currentPage: page,
         limit,
         search,
-        filter,
         categories,
         statuses,
-        sortable,
-        searchable,
-        filterable,
+        categoryOptions,
+        statusOptions,
+        baseUrl: '/organizer-inventory'
     };
 }
 
