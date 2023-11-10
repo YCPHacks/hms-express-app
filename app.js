ddf9e2a3d88ac2d3f3769af1491fa9b0646bd941
kjacoby3 <kjacoby3@ycp.edu>
2023-11-10 10:04:43 -0500

    add fastify routes

--- app.js
+++ app.js
@@ -34,24 +34,3 @@ module.exports = async function (fastify, opts) {
     options: Object.assign({}, opts)
   })
 }
-
-//  // Express App
-
-// const app = express();
-
-// app.use(router);
-
-// const port = process.env.PORT;
-
-// app.set('view engine', 'pug');
-// app.set('views', './views');
-
-// app.use('/static', express.static('./public'));
-
-// app.get('/', (req, res) => {
-//   res.status(200).send('OK');
-// });
-
-// app.listen(port, () => {
-//   console.log(`Listening on port ${port}...`);
-// });
